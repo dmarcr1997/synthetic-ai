fd10a01e85aa97592abb500e29dd25d25ce06a33
Damon Marc Rocha <dmarcr1997@gmail.com>
2020-05-08 00:42:03 -0500

    worked on user controller

--- SyntheticAi_backend/app/controllers/users_controller.rb
+++ SyntheticAi_backend/app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+    def create
+        user = User.create(user_params)
+        if user.save
+            render json: UserSerializer.new(user)
+        else
+            render json: {message: 'error logging in'}
+        end
+    end
+
+    def show
+        user = user.find(params[:id])
+        render json: UserSerializer.new(user)
+    end
+
+    private
+
+    def user_param
+        params.require(:user).permit(:username, email, password)
+    end
 end
--- SyntheticAi_backend/config/routes.rb
+++ SyntheticAi_backend/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :brains
-  resources :users
+  
+  resources :users, only: [:create, :show] do
+    resources :brains, only: [:show, :create, :index, :edit]
+  end
+  resources :sessions, only: [:show, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
